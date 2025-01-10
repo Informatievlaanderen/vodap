9787bb0db94b48acec772ce889579c5d0392534e
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2025-01-10 15:51:43 +0000

    update download turtle

--- scripts/download_turtle.sh
+++ scripts/download_turtle.sh
@@ -3,14 +3,20 @@
 # a curl version of the OSLO-Hypermedia driven pagination building block
 # returning a ntriples file
 # 
+set -x
 
 URL=$1
 FORMAT="application/rdf+xml"
 TARGET=$2
 
+startindex=0
+
+
 # ensure target directory exists
 DIR=$(dirname ${TARGET})
 mkdir -p ${DIR}
+
+TARGETFILE=$(basename ${TARGET})
 
 
 #####################################
@@ -50,6 +56,43 @@ done
 
 }
 
+#####################################
+# process as if there where many pages 
+#####################################
+# https://metadata.vlaanderen.be/api/collections/main/items/?f=dcat_ap_vl
+# https://metadata.vlaanderen.be/api/collections/main/items/items?startindex=10&limit=10
+process_pages() {
+
+BASE=$1
+PAGESIZE=$2
+STARTPARAM=startindex
+PAGESIZEPARAM=limit
+
+i=0
+ERR=200
+CERR=/tmp/CERR
+WKD=/tmp/workspace/
+
+
+while [ $ERR -lt 400 ] ; do
+    URL="${BASE}&${STARTPARAM}=${i}&${PAGESIZEPARAM}=${PAGESIZE}"
+    PREFIX="${STARTPARAM}${i}.${PAGESIZEPARAM}${PAGESIZE}."
+    echo "process ${URL}" 
+    curl -s -w "%{stderr}%{http_code}" --retry 5 -D headers -o payload -k -L "${URL}" &> CERR
+    rapper -i guess -o turtle payload > "${WKD}${PREFIX}${TARGETFILE}"
+    if [ $? -eq 0 ] ; then 
+	    cat "${WKD}${PREFIX}${TARGETFILE}" >> ${TARGET}
+	    rm "${WKD}${PREFIX}${TARGETFILE}" 
+    fi
+    i=$((i+${PAGESIZE}))
+    ERR=$(cat CERR )
+    echo $ERR
+
+done
+
+}
+
+
 
 ######################################
 # curl
@@ -82,7 +125,9 @@ if [ "${FIRST}" != "" -a "${NEXT}" != "" -a "${LAST}" != "" ] ; then
   exit 0;
 fi
 
+echo "no pagination found - try hardcoded pagination"
+process_pages ${URL} 50 ${TARGET}
 
-echo "no pagination found - assume single respons"
-rapper -i guess payload > ${TARGET}
+#echo "no pagination found - assume single respons"
+#rapper -i guess payload > ${TARGET}
 exit 0;
